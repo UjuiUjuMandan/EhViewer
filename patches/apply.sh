067beea569d9449ba7354e5810596980a0dc7427
ᡠᠵᡠᡳ ᡠᠵᡠ ᠮᠠᠨᡩ᠋ᠠᠨ <125150101+UjuiUjuMandan@users.noreply.github.com>
2025-12-16 06:23:21 +0000

    pick 935b7de & 4e0c42c

--- patches/apply.sh
+++ patches/apply.sh
@@ -5,3 +5,5 @@ git apply ../patches/0001-anti-dpi.patch
 source ../patches/0002-disable-cronet.sh
 source ../patches/0003-disable-autoupdate.sh
 source ../patches/0004-disable-cpp-build-id.sh
+git apply ../patches/935b7de.patch
+git apply ../patches/4e0c42c.patch
