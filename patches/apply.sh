28a290505afb105bcd743437ab8d1acf698d7f35
ᡠᠵᡠᡳ ᡠᠵᡠ ᠮᠠᠨᡩ᠋ᠠᠨ <125150101+UjuiUjuMandan@users.noreply.github.com>
2025-12-16 07:22:24 +0000

    change application id

--- patches/apply.sh
+++ patches/apply.sh
@@ -7,3 +7,4 @@ source ../patches/0003-disable-autoupdate.sh
 source ../patches/0004-disable-cpp-build-id.sh
 git apply ../patches/935b7de.patch
 git apply ../patches/4e0c42c.patch
+sed -i -e 's/applicationId = "moe.tarsin.ehviewer"/applicationId = "moe.tarsin.ehviewer.cc"/' app/build.gradle.kts
