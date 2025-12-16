4b1b2d0abd52bbad7cbb43a5fcad7c75e7be3119
ᡠᠵᡠᡳ ᡠᠵᡠ ᠮᠠᠨᡩ᠋ᠠᠨ <125150101+UjuiUjuMandan@users.noreply.github.com>
2025-12-16 10:40:15 +0000

    Revert "Disable cpp build-id"
    This reverts commit bddf3b7e180dadfd8a6a0e7186540e9a7c4d99e4.

--- patches/0004-disable-cpp-build-id.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i '/project(ehviewer C)/a\add_link_options("-Wl,--build-id=none")' app/src/main/cpp/CMakeLists.txt
--- patches/apply.sh
+++ patches/apply.sh
@@ -4,7 +4,6 @@
 git apply ../patches/0001-anti-dpi.patch
 source ../patches/0002-disable-cronet.sh
 source ../patches/0003-disable-autoupdate.sh
-source ../patches/0004-disable-cpp-build-id.sh
 git apply ../patches/935b7de.patch
 git apply ../patches/4e0c42c.patch
 sed -i -e 's/applicationId = "moe.tarsin.ehviewer"/applicationId = "moe.tarsin.ehviewer.cc"/' app/build.gradle.kts
