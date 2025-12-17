04a9ef346f327bb76ee96967c42928d9542f7a0d
ᡠᠵᡠᡳ ᡠᠵᡠ ᠮᠠᠨᡩ᠋ᠠᠨ <125150101+UjuiUjuMandan@users.noreply.github.com>
2025-12-17 22:32:51 +0800

    1.14.6

--- patches/apply.sh
+++ patches/apply.sh
@@ -4,6 +4,4 @@
 git apply ../patches/0001-anti-dpi.patch
 source ../patches/0002-disable-cronet.sh
 source ../patches/0003-disable-autoupdate.sh
-git apply ../patches/935b7de.patch
-git apply ../patches/4e0c42c.patch
 sed -i -e 's/applicationId = "moe.tarsin.ehviewer"/applicationId = "moe.tarsin.ehviewer.cc"/' app/build.gradle.kts
