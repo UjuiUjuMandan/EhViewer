4cf956124b52fb7c216f5c6b2f2b7eb335a6f307
ᡠᠵᡠᡳ ᡠᠵᡠ ᠮᠠᠨᡩ᠋ᠠᠨ <125150101+UjuiUjuMandan@users.noreply.github.com>
2025-12-18 00:47:28 +0000

    enableQuic to disable OkHttp

--- patches/0002-disable-cronet.sh
+++ patches/0002-disable-cronet.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sed -i 's/Settings.enableCronet.value/false/' app/src/main/kotlin/com/hippo/ehviewer/EhApplication.kt
+sed -i 's/Settings.enableCronet.value/Settings.enableQuic.value/' app/src/main/kotlin/com/hippo/ehviewer/EhApplication.kt
+sed -i 's/"enable_quic", true/"enable_quic", false/' app/src/main/kotlin/com/hippo/ehviewer/Settings.kt
