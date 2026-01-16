dda01ec3219ca4ceca05fb55cd622d6303cf7930
ᡠᠵᡠᡳ ᡠᠵᡠ ᠮᠠᠨᡩ᠋ᠠᠨ <125150101+UjuiUjuMandan@users.noreply.github.com>
2026-01-16 05:31:04 +0000

    Increase requestTimeoutMillis instead

---
+++ patches/0004-increase-request-timeout.sh
@@ -0,0 +1,1 @@
+sed -i 's/requestTimeoutMillis = 10_000/requestTimeoutMillis = 30_000/' app/src/main/kotlin/com/hippo/ehviewer/ktor/Common.kt
--- patches/0004-timeout.sh
+++
@@ -1,1 +0,0 @@
-sed -i 's/requestTimeoutMillis = 10_000/connectTimeoutMillis = 10_000/' app/src/main/kotlin/com/hippo/ehviewer/ktor/Common.kt
